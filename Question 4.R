f0268b5d93feda3ed9ca12e70233585d256a4997
olrikj <olrik2@hotmail.com>
2025-01-24 16:39:49 +0100

    Question 4:

---
+++ Question 4.R
@@ -0,0 +1,12 @@
+#Question 4 :
+
+##Création de la fonction :
+
+compter_nombre_d_adjoints <- function(x){
+  Premier_adjoint <- sum(x$Libellé.de.la.fonction == "1er adjoint au Maire")
+  Deuxième_adjoint <- sum(x$Libellé.de.la.fonction == "2ème adjoint au Maire")
+  Troisième_adjoint <- sum(x$Libellé.de.la.fonction == "3ème adjoint au Maire")
+  return(Premier_adjoint+Deuxième_adjoint+Troisième_adjoint)
+}
+
+compter_nombre_d_adjoints(df_Gers)
