86d4d2e7113ae116f259ffdc9c048ebc439a292b
olrikj <olrik2@hotmail.com>
2025-02-03 11:34:09 +0100

    Question 7

--- Question 7.R
+++ Question 7.R
@@ -1,2 +1,22 @@
 #Question 7
 
+plot_code_professions <- function(df, title = "Répartition des élus par code professionnel") {
+  if (!"Code.de.la.catégorie.socio.professionnelle" %in% colnames(df)) {
+    stop("Le dataframe ne contient pas de colonne 'Code.de.la.catégorie.socio.professionnelle'.")
+  }
+  
+  # Compter le nombre d'élus par code professionnel
+  counts <- table(df$Code.de.la.catégorie.socio.professionnelle)
+  
+  # Triez les codes par nombre d'élus (du plus fréquent au moins fréquent)
+  counts <- sort(counts, decreasing = TRUE)
+  
+  # Création du bar chart horizontal
+  barplot(counts, horiz = TRUE, las = 1, col = "steelblue",
+          main = title, xlab = "Nombre d'élus", ylab = "Code Professionnel")
+}
+
+
+
+plot_code_professions(df_Gers)
+
